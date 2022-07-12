b3a5bdcf4e39f9d977746db7e0a0e67739a5f7de
Jessie Luo <jluo30@jhu.edu>
2022-07-11 20:56:30 -0700

    collect cityname, temperature, condition

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -21,7 +21,6 @@ struct WeatherManager {
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
         performRequest(urlString: urlString)
     }
 
@@ -32,32 +31,41 @@ struct WeatherManager {
 
     func performRequest(urlString: String) {
         if let url = URL(string: urlString) {
-            print("1")
             let session = URLSession(configuration: .default)
             let task = session.dataTask(with: url){(data, response, error) in
                 if error != nil {
-                    print(error!)
                     return
                 }
                 if let safeData = data {
-                    self.parseJSON(safeData)
+//                    if let weather = self.parseJSON(safeData) {
+//
+//                    }
                 }
             }
             task.resume()
         } else {
-            print("2")
         }
     }
     
-    func parseJSON(_ weatherData: Data) {
+    func parseJSON(_ weatherData: Data) -> WeatherModel {
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.main.temp)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+//            print(weather.conditionName)
+//            print(weather.temperatureString)
+            return weather
+            
         } catch {
             print(error)
+            return nil
         }
     }
+    
+
     
 //    func parseJSON(_ weatherData: Data) {
 //            let decoder = JSONDecoder()
